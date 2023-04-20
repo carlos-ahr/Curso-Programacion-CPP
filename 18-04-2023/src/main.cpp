1b7b1b00fdb6e0b2724bb6a104785bbb0122ab5d
carlos-ahr <89262680+carlos-ahr@users.noreply.github.com>
2023-04-20 02:19:43 +0000

    18-04-2023

--- 18-04-2023/src/main.cpp
+++ 18-04-2023/src/main.cpp
@@ -1,9 +1,9 @@
+#include <iostream>
 
 using namespace std;
-int main(){
-    int integer = 999999;
-    float flt = 999999.999999;
-    char character = 'F';
-    bool boolean = false;
-};
 
+int main()
+{
+    cout << "Hola mundo" << endl;
+    return 0;
+}
